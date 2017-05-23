b1ee3a96ec089770092742471cda96d460cc79ef
dandgerson <dandgerson@gmail.com>
2017-05-23 19:02:51 +0300

    header menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,49 @@
+// Parallax adaptation for new version jQuery
+
+$(document).ready(function () {
+	$('[data-parallax="scroll"]').parallax();
+});
+
+// Resize header
+
 $(document).ready(function(){
 
 	function heightDetect() {
 		$(".main-head").css('height', $(window).height());
 	}
+
 	heightDetect();
+	
 	$(window).resize(function() {
 		heightDetect();
 	});
 
-	$('.main-head').parallax({imageSrc: '../img/bg.png'});
-
 	console.log(1)
 }); 
+
+// Preloader
 
 $(window).ready(function() { 
 	$(".loader-inner").fadeOut(); 
 	$(".loader").delay(400).fadeOut("slow"); 
+});
+
+// Sandwich button and toggle menu
+
+$(document).ready(function(){
+
+// sandwich button
+	$(".sandwich, .menu_item").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+// toggle menu
+	$(".sandwich").click(function() {
+		if ($(".top-menu").is(":visible")) {
+			$(".top-menu").fadeOut(600);
+			} else {
+				$(".top-menu").fadeIn(600);
+			};
+	});
+
 });
