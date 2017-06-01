37bcb23548e7e6092f2a4cb95f5487699b3bbcd8
dandgerson <dandgerson@gmail.com>
2017-06-01 15:18:01 +0300

    theming and wordpress

--- app/js/main.js
+++ app/js/main.js
@@ -1,38 +1,31 @@
-// Parallax adaptation for new version jQuery
+
 
 $(document).ready(function () {
+
+
+	// Parallax adaptation for new version jQuery
 	$('[data-parallax="scroll"]').parallax();
-});
 
-
-// Portfolio
-
-$(document).ready(function () {
-
+	// Portfolio
 	$(".s-portfolio li").click(function() {
 		$(".s-portfolio li").removeClass('active');
 		$(this).addClass('active');
 	});
 
-// mixitup
-var containerEl = document.querySelector('.p-container');
-var mixer = mixitup(containerEl);
+	// mixitup
+	var containerEl = document.querySelector('.p-container');
+	var mixer = mixitup(containerEl);
+	$(".portfolio-item").each(function(i) {
+		$(this).find("a").attr("href", "#work-" + i);
+		$(this).find(".port-descr").attr("id", "work-" + i);
+	});
 
-});
-
-// Animate + Waypoint
-
-$(document).ready(function(){
-
+	// Animate + Waypoint
 	$(".top-text h1").animated("fadeInDown", "fadeOut");
 	$(".top-text p").animated("fadeInUp", "fadeOut");
-	
+
 	$(".top-text h1").css("animation-delay","1s");
 	$(".top-text p").css("animation-delay","1.8s");
-
-});
-
-$(document).ready(function(){
 
 	$(".h-about").animated("fadeInUp", "fadeOutDown");
 
@@ -40,7 +33,7 @@ $(document).ready(function(){
 	$(".animation-about-1").css("animation-delay",".8s");
 	$(".animation-about-2").animated("fadeInLeft", "fadeOut");
 	$(".animation-about-3").animated("fadeInRight", "fadeOut");
-	
+
 	$(".h-portfolio").animated("fadeInUp", "fadeOutDown");
 	$(".h-contacts").animated("fadeInUp", "fadeOutDown");
 
@@ -57,11 +50,8 @@ $(document).ready(function(){
 	$(".r-ri-4").animated("fadeInRight", "fadeOut");
 
 	$(".resume-item").css("animation-delay",".8s");
-});
-// Resize header
 
-$(document).ready(function(){
-
+	// Resize header
 	function heightDetect() {
 		$(".main-head").css('height', $(window).height());
 	}
@@ -72,8 +62,48 @@ $(document).ready(function(){
 		heightDetect();
 	});
 
-	console.log(1)
-}); 
+	// sandwich button
+	$(".sandwich").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".menu-item").click(function() {
+		$(".top-menu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+	});
+
+	// sandwich
+
+	$(".sandwich").click(function() {
+        if (!$(".top-menu").is(":visible")) {
+            $(".top-text").addClass("h-opacify");
+            $(".top-menu").fadeIn(600);
+            $(".top-menu li a").addClass("fadeInUp animated");
+        } else {
+            $(".top-text").removeClass("h-opacify");
+            $(".top-menu").fadeOut(600);
+            $(".top-menu li a").removeClass("fadeInUp animated");
+        }
+    });
+
+	$(".top-menu ul a").click(function() {
+		$(".top-text").removeClass("h-opacify");
+	});﻿
+
+	// Magnific popup
+	$(".popup").magnificPopup({type:"image"});
+	$(".popup-content").magnificPopup({
+		type: "inline",
+		midClick: true
+	});
+	// jqBootstrapValidation
+	$("input, select, textarea").jqBootstrapValidation();
+
+	// mPageScroll2id
+	$(".top-menu ul a").mPageScroll2id();
+
+
+});
 
 // Preloader
 
@@ -81,61 +111,3 @@ $(window).ready(function() {
 	$(".loader-inner").fadeOut(); 
 	$(".loader").delay(400).fadeOut("slow"); 
 });
-
-// Sandwich button and toggle menu
-
-$(document).ready(function(){
-
-// sandwich button
-$(".sandwich").click(function() {
-	$(".sandwich").toggleClass("active");
-});
-
-$(".menu-item").click(function() {
-	$(".top-menu").fadeOut(600);
-	$(".sandwich").toggleClass("active");
-});
-
-// sandwich
-
-$(".sandwich").click(function() {
-	if ($(".top-menu").is(":visible")) {
-		$(".top-text").removeClass("h-opacify");
-		$(".top-menu").fadeOut(600);
-		$(".top-menu li a").removeClass("fadeInUp animated");
-	} else {
-		$(".top-text").addClass("h-opacify");
-		$(".top-menu").fadeIn(600);
-		$(".top-menu li a").addClass("fadeInUp animated");
-	};
-});
-
-$(".top-menu ul a").click(function() {
-	$(".top-text").removeClass("h-opacify");
-});﻿
-});
-
-// Magnific popup
-
-$(document).ready(function() {
-	$(".popup").magnificPopup({type:"image"});
-	$(".popup-content").magnificPopup({
-		type: "inline",
-		midClick: true
-	});
-});
-
-$(document).ready(function(){
-
-	$(".portfolio-item").each(function(i) {
-		$(this).find("a").attr("href", "#work-" + i);
-		$(this).find(".port-descr").attr("id", "work-" + i);
-	});
-
-	$("input, select, textarea").jqBootstrapValidation();
-
-	 $(".top-menu ul a").mPageScroll2id();
-	 
-
-});
-
